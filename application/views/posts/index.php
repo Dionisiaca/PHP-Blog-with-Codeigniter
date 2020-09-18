9dde4bf3bf04fd5c9b563535b5b32329a31188f5
claudiacustodio <claudia@4040apps.com>
2020-09-18 17:28:57 -0500

    Truncate text in posts preview

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -2,6 +2,6 @@
 <?php foreach ($posts as $post) : ?>
     <h3><?php echo $post['title']; ?></h3>
     <small>Posted on: <?php echo $post['created_at']; ?></small>
-    <p><?php echo $post['body']; ?></p>
+    <p><?php echo word_limiter($post['body'], 150); ?></p>
     <a class='btn btn-default' href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read more</a>    
 <?php endforeach; ?>
