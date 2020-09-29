b46b7c4a42731636cea2d260d01a4b3c7ef8338b
claudiacustodio <claudia@4040apps.com>
2020-09-29 17:12:07 -0500

    create and filter categories

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -57,4 +57,12 @@
 
             return $query->result_array();
         }
+
+        public function get_posts_by_category($id){
+            $this->db->order_by('posts.id', 'DESC');
+            $this->db->join('categories', 'categories.id = posts.category_id');
+            $query = $this->db->get_where('posts', array('category_id' => $id));
+
+            return $query->result_array();
+        }
     }
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -18,14 +18,14 @@
                     <li><a href="<?php echo base_url(); ?>">Home</a></li>
                     <li><a href="<?php echo base_url(); ?>about">About</a></li>
                     <li><a href="<?php echo base_url(); ?>posts">Posts</a></li>
+                    <li><a href="<?php echo base_url(); ?>categories">Categories</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right">
-                <li><a href="<?php echo base_url(); ?>posts/new">Create post</a></li>
-
+                    <li><a href="<?php echo base_url(); ?>posts/new">Create post</a></li>
+                    <li><a href="<?php echo base_url(); ?>categories/create">Create category</a></li>
                 </ul>
             </div>
         </div>
     </nav>
 
     <div class="container">
-
